fix issue with string/numerical columns in plotly text function

ds_viz/_plotlytools.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 def get_iplot_text(data, names=None, round_digits=2):
@@ -6,10 +7,16 @@ def get_iplot_text(data, names=None, round_digits=2):
     if names is None:
         if hasattr(data, 'columns'):
             names = data.columns.tolist()
-            data = np.round(data.values, round_digits)
+            num_cols = [col for col in data.columns if data[col].dropna().shape[0] > 0 and
+                        str(data[col].dropna().iloc[0]).replace(".", "").replace("-", '').replace("e", '').isnumeric()]
+            data[num_cols] = data[num_cols].round(round_digits)
+
         else:
-            data = np.round(np.array(data), round_digits)
             names = [f'var{i + 1}' for i in range(data.shape[1])]
+            data = pd.DataFrame(columns=names, data=data)
+            num_cols = [col for col in data.columns if data[col].dropna().shape[0] > 0 and
+                        str(data[col].dropna().iloc[0]).replace(".", "").replace("-", '').replace("e", '').isnumeric()]
+            data[num_cols] = data[num_cols].round(round_digits)
 
     text_array = np.char.array([names[0]] * data.shape[0])
     text_array = text_array + ": " + np.char.array(data[:, 0], unicode=True) + "<br>"