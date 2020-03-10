492724314c3520fc01127a4d39b0d20b74fad171
Leon T Johnson <johnson.leon.t@gmail.com>
2020-03-10 13:56:31 -0400

    deprecate datatype coercing. made no sense.

--- ds_db/_basicsql.py
+++ ds_db/_basicsql.py
@@ -3,24 +3,7 @@ import pyodbc
 import os
 import pandas as pd
 import numpy as np
-from sqlalchemy import create_engine, DATETIME, INTEGER, FLOAT, BIGINT, VARCHAR, BOOLEAN
-
-
-def get_dtype(pandas_dtype):
-    if pandas_dtype in ['object', 'str']:
-        return VARCHAR
-    elif pandas_dtype in ['bool']:
-        return BOOLEAN
-    elif pandas_dtype in ['datetime64[ns]']:
-        return DATETIME
-    elif pandas_dtype in ['int32']:
-        return INTEGER
-    elif pandas_dtype in ['int64']:
-        return BIGINT
-    elif pandas_dtype in ['float64', 'float32']:
-        return FLOAT
-    else:
-        return VARCHAR
+from sqlalchemy import create_engine
 
 
 class SQLConnection(object):
@@ -51,8 +34,7 @@ class SQLConnection(object):
                                          user=username,
                                          password=password)
 
-    def upload_df(self, df, target_table, schema='dbo', if_table_exists='fail', to_sql_kws=None, preprocess_func=None,
-                  coerce_dtypes=False):
+    def upload_df(self, df, target_table, schema='dbo', if_table_exists='fail', to_sql_kws=None, preprocess_func=None):
         '''
         Upload a pandas DataFrame into a SQL database.
 
@@ -70,9 +52,6 @@ class SQLConnection(object):
         to_sql_kws : dict
 
         preprocess_func : function
-
-        coerce_dtypes: bool
-            Use get_dtype function (in dstools.ds_db) to determine columns' data type
 
         Returns
         -------
@@ -92,10 +71,14 @@ class SQLConnection(object):
         if preprocess_func is not None:
             df = preprocess_func(df)
 
-        df.replace([np.nan], [None], inplace=True)
+        nan_values = [np.nan, '  ', ' ', '', 'null', 'nan', 'NA', '-', '--']
+        df.replace(nan_values, [None] * len(nan_values), inplace=True)
 
-        if coerce_dtypes:
-            to_sql_kws['dtype'] = {col: get_dtype(df[col].dtype) for col in df}
+        # Limit chunk size for SQL server ...
+        if 'chunksize' in to_sql_kws.keys() and 'SQL Server' in self.driver:
+            df_num_of_cols = len(df.columns)
+            chunksize = int(np.floor(2100 / df_num_of_cols) - 1)
+            to_sql_kws['chunksize'] = min(chunksize, to_sql_kws['chunksize'])
 
         print(f"Loading table (shape {df.shape}) into {target_table}. If the table exists, {if_table_exists}.")
         df.to_sql(target_table, schema=schema, con=engine, if_exists=if_table_exists, **to_sql_kws)
@@ -134,7 +117,7 @@ class SQLConnection(object):
                               (self.df_schema.column_name.str.lower().str.contains(in_column_name.lower()))]
 
     def upload_folder(self, folder_path, target_table=None, schema='dbo', csv=True, preprocess_func=None,
-                      read_csv_kws=None, to_sql_kws=None, sort_columns=True, coerce_dtypes=False):
+                      read_csv_kws=None, to_sql_kws=None, sort_columns=True):
         if target_table is None:
             target_table = folder_path[folder_path.rfind('/') + 1:]
 
@@ -153,7 +136,7 @@ class SQLConnection(object):
 
             print(f"[{i + 1} of {len(files)}] Appending {file} to {target_table} ...")
             self.upload_df(df, target_table, schema=schema, if_table_exists='append', to_sql_kws=to_sql_kws,
-                           preprocess_func=preprocess_func, coerce_dtypes=coerce_dtypes)
+                           preprocess_func=preprocess_func)
 
     def __del__(self):
         self.connection.close()
